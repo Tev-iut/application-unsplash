6f31a8c18d4a64a73104207ba783dc22b8e58677
Tévin DERVAUX <tevin.dervaux@etu.univ-savoie.fr>
2024-01-23 13:43:38 +0100

    Initial Commit

---
+++ Application Unsplash/Application_UnsplashApp.swift
@@ -0,0 +1,17 @@
+//
+//  Application_UnsplashApp.swift
+//  Application Unsplash
+//
+//  Created by Tevin DERVAUX on 1/23/24.
+//
+
+import SwiftUI
+
+@main
+struct Application_UnsplashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
