ba2d3d0968c00e233898386e69a6e56abb053914
Tévin DERVAUX <tevin.dervaux@etu.univ-savoie.fr>
2024-01-24 09:40:40 +0100

    Ajout des réponses aux questions

--- Application Unsplash/ContentView.swift
+++ Application Unsplash/ContentView.swift
@@ -20,19 +20,30 @@ extension Image {
 }
 
 struct ContentView: View {
+    @State var imageList: [UnsplashPhoto] = []
     
-    let imageURLs: [String] = [
-        "https://images.unsplash.com/photo-1683009427666-340595e57e43?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1MzYyNDN8MXwxfGFsbHwxfHx8fHx8MXx8MTcwMzc1OTU1MXw&ixlib=rb-4.0.3&q=80&w=1080",
-        "https://images.unsplash.com/photo-1563473213013-de2a0133c100?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1MzYyNDN8MHwxfGFsbHwyfHx8fHx8MXx8MTcwMzc1OTU1MXw&ixlib=rb-4.0.3&q=80&w=1080",
-        "https://images.unsplash.com/photo-1490349368154-73de9c9bc37c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1MzYyNDN8MHwxfGFsbHwzfHx8fHx8MXx8MTcwMzc1OTU1MXw&ixlib=rb-4.0.3&q=80&w=1080",
-        "https://images.unsplash.com/photo-1495379572396-5f27a279ee91?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1MzYyNDN8MHwxfGFsbHw0fHx8fHx8MXx8MTcwMzc1OTU1MXw&ixlib=rb-4.0.3&q=80&w=1080",
-        "https://images.unsplash.com/photo-1560850038-f95de6e715b3?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1MzYyNDN8MHwxfGFsbHw1fHx8fHx8MXx8MTcwMzc1OTU1MXw&ixlib=rb-4.0.3&q=80&w=1080",
-        "https://images.unsplash.com/photo-1695653422715-991ec3a0db7a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1MzYyNDN8MXwxfGFsbHw2fHx8fHx8MXx8MTcwMzc1OTU1MXw&ixlib=rb-4.0.3&q=80&w=1080",
-        "https://images.unsplash.com/photo-1547327132-5d20850c62b5?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1MzYyNDN8MHwxfGFsbHw3fHx8fHx8MXx8MTcwMzc1OTU1MXw&ixlib=rb-4.0.3&q=80&w=1080",
-        "https://images.unsplash.com/photo-1492724724894-7464c27d0ceb?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1MzYyNDN8MHwxfGFsbHw4fHx8fHx8MXx8MTcwMzc1OTU1MXw&ixlib=rb-4.0.3&q=80&w=1080",
-        "https://images.unsplash.com/photo-1475694867812-f82b8696d610?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1MzYyNDN8MHwxfGFsbHw5fHx8fHx8MXx8MTcwMzc1OTU1MXw&ixlib=rb-4.0.3&q=80&w=1080",
-        "https://images.unsplash.com/photo-1558816280-dee9521ff364?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1MzYyNDN8MHwxfGFsbHwxMHx8fHx8fDF8fDE3MDM3NTk1NTF8&ixlib=rb-4.0.3&q=80&w=1080"
-    ]
+    func loadData() async {
+        // Créez une URL avec la clé d'API
+        let url = URL(string: "https://api.unsplash.com/photos?client_id=\(ConfigurationManager.instance.plistDictionnary.clientId)")!
+        
+        do {
+            // Créez une requête avec cette URL
+            let request = URLRequest(url: url)
+            
+            // Faites l'appel réseau
+            let (data, response) = try await URLSession.shared.data(for: request)
+            
+            // Transformez les données en JSON
+            let deserializedData = try JSONDecoder().decode([UnsplashPhoto].self, from: data)
+            
+            // Mettez à jour l'état de la vue
+            imageList = deserializedData
+            
+        } catch {
+            print("Error: \(error)")
+        }
+    }
+    
     
     let columns = [
         GridItem(.adaptive(minimum: 150)),
@@ -40,11 +51,19 @@ struct ContentView: View {
     ]
     
     var body: some View {
+        
         NavigationStack {
+            Button(action: {
+                   Task {
+                       await loadData()
+                   }
+               }, label: {
+                   Text("Load Data")
+               })
             ScrollView {
                 LazyVGrid(columns: columns, spacing: 8) {
-                    ForEach(imageURLs, id: \.self) { url in
-                        AsyncImage(url: URL(string: url)) { image in
+                    ForEach(imageList, id: \.id) { photo in
+                        AsyncImage(url: URL(string: photo.urls.full)!) { image in
                             image.centerCropped().frame(height: 150).cornerRadius(10.0)
                         } placeholder: {
                             Color.gray.frame(height: 150)
---
+++ answers.md
@@ -0,0 +1,9 @@
+Tévin DERVAUX
+
+# 4 - Appels réseaux
+
+### Exercice 3
+
+Async / await permet de définir des méthodes asynchrones.   
+Combine utilise des publishers et des subsribers.  
+completionHandler / GCD permet de dispatcher les tâches sur différents threads.
